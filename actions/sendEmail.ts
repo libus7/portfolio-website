f064aa5f80220c6a765a03d2e9ed50de66da6bce
matt.libby <matt.libby@fruition.net>
2024-04-12 21:30:20 -0600

    first commit

---
+++ actions/sendEmail.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { Resend } from "resend";
+import { getErrorMessage, validateString } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
+import { data } from "autoprefixer";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get('senderEmail');
+  const message = formData.get("message");
+
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email"
+    };
+  }
+
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message"
+    };
+  }
+
+  try {
+      await resend.emails.send({
+        from: 'Contact Form <onboarding@resend.dev>',
+        to: 'libus7@gmail.com',
+        subject: 'Message from contact form',
+        reply_to: senderEmail as string,
+        react: React.createElement(ContactFormEmail, {
+          message: message as string,
+          senderEmail: senderEmail as string,
+        }),
+      });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error)
+    };
+  }
+  return {
+    data,
+  };
+};
